16487c5eeed19e965fbdf4494aabc338c1373be8
el-uhu <luki.hutter@gmail.com>
2015-07-10 10:20:54 +0100

    adaption of continueSim-function to work with stochastic models

--- utils.jl
+++ utils.jl
@@ -14,7 +14,15 @@ function restoreModel!(M)
     return(M)
 end
 
-function continueSim!(M, t1, t2, name, inits2, pars2)
+#function to find t corresponding to desired tr
+function find_t_for_tr(M, simname, query_time)
+    T = M.sims[simname].D["t"]
+    TR = M.sims[simname].D["tr"]
+    total = [findfirst([tr >= query_time for tr in TR])]
+    return(total)
+end
+
+function continueSim!(M, t1, t2, name, pars2; inits2 = false, stochastic = false)
     M.spec["total"] = t1
     n1 = name * "_1"
     n2 = name * "_2"
@@ -28,8 +36,11 @@ function continueSim!(M, t1, t2, name, inits2, pars2)
     M.pars = pars2
     M.spec["total"] = t2
     runSimulation!(M, n2)
-    M.sims[n2].D["t"] = M.sims[n2].D["t"] + t1
-    M.sims[name] = SimulationData(M)
+    M.sims[n2].D["t"] = M.sims[n2].D["t"] .+ t1
+    if stochastic == true
+        M.sims[n2].D["tr"] = M.sims[n2].D["tr"] .+ M.sims[n1].D["tr"][end]
+    end
+    M.sims[name] = SimulationData(M, name)
     for v in keys(M.sims[name].D)
         append!(M.sims[name].D[v], M.sims[n1].D[v])
         append!(M.sims[name].D[v], M.sims[n2].D[v])
